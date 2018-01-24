454c36a18390ebbbbe64998df4b655dd760da110
umesh132200 <umesh132200@gmail.com>
2018-01-24 11:52:48 +0530

    it's working

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { forkJoin } from "rxjs/observable/forkJoin";
+//import {parseString} from "xml2js";
+//import * as x2js  from 'xml2js';
 declare var $:any;
 
 @Component({
@@ -11,24 +15,29 @@ export class WeatherComponent implements OnInit {
 
   constructor(private httpClient:HttpClient) { }
   arr:any[];
-  detail:any[]=[];
+  detail1:any[]=[];
+  detail2:any[]=[];
   cityName:any[];
   weatherDes:any[];
   sunRise:any[];
   sunSet:any[];
   temp:any[];
-  tableData:any[] = new Array();  
+  tableData:any[] = new Array();
+  tdata:any[] = new Array();  
   weatherData:any='';
+  cityWiseData:any='';
 
   ngOnInit() {
     let x2js = new X2JS();
-    this.httpClient.get('http://api.openweathermap.org/data/2.5/find?lat=27.5&lon=77.5&cnt=5&mode=xml&appid=a71be9c0523a5505e58265eb91dbca4f',{responseType:"text"})
-    .subscribe(
-    (res =>{
+    let cityWeather = this.httpClient.get('http://api.openweathermap.org/data/2.5/find?lat=27.5&lon=77.5&cnt=5&mode=xml&appid=a71be9c0523a5505e58265eb91dbca4f',{responseType:"text"});
+    let dayWeather = this.httpClient.get('https://cors-anywhere.herokuapp.com/http://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b6907d289e10d714a6e88b30761fae22');
+   
+    forkJoin([cityWeather,dayWeather]).subscribe(
+    (response =>{
       //Convert Xml string to JSON
-      this.weatherData = x2js.xml_str2json(res);  
+      this.weatherData = x2js.xml_str2json(response[0]);  
      
-      //To access all array item
+      //To access all array item for city weather
       this.weatherData.cities.list.item.forEach((item, i:number) => {
          this.cityName = this.weatherData.cities.list.item[i].city._name;
          this.weatherDes = this.weatherData.cities.list.item[i].weather._value;
@@ -38,7 +47,23 @@ export class WeatherComponent implements OnInit {
          this.arr =[this.cityName,this.weatherDes,this.sunRise,this.sunSet,this.temp];
          this.tableData.push(this.arr);  
         });
-        this.detail= this.tableData;
+      this.detail1= this.tableData;
+
+      //To access all array item for day wise weather
+      this.cityWiseData = response[1];
+      this.cityWiseData.list.forEach((items, j:number)=>{
+        let time:string[] = this.cityWiseData.list[j].dt_txt;
+        let des:string[] = this.cityWiseData.list[j].weather["0"].description; 
+        let temporature:string[] = this.cityWiseData.list[j].main.temp; 
+        let min_temp:string[] = this.cityWiseData.list[j].main.temp_min;
+        let pressure:string[] = this.cityWiseData.list[j].main.pressure;  
+        let arr = [time, des, temporature,min_temp, pressure];
+        this.tdata.push(arr);
+      });
+       this.detail2 = this.tdata;
+        console.log(this.tdata);
+        //console.log(this.weatherData);
+        //console.log(this.cityWiseData);
     })  
     );
 
