c103251f7eda4441e23f4ec3a91a18b85770e00e
umesh132200 <umesh132200@gmail.com>
2018-01-29 16:54:59 +0530

    City search & select updated

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -12,8 +12,7 @@ declare var $:any;
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent implements OnInit {
-
-  constructor(private httpClient:HttpClient, private dataFinder:DataFinder) { }
+  constructor(private httpClient:HttpClient, private dataFinder:DataFinder) {}
   arr:any[];
   detail1:any[]=[];
   detail2:any[]=[];
@@ -34,39 +33,20 @@ export class WeatherComponent implements OnInit {
   results = [];
   cityData = [];
   arrData =[];
-  searchCity(){
-    let city:string = $('#city').val();
-    var searchField = "name";
-    this.dataFinder.getJSONDataAsync("./assets/data/city.list.json").then(data => {
-    for (var i=0 ; i < data.length ; i++) {
-        if (data[i]['name'] == city) {
-            if(data[i].id && data[i].name && data[i].country && data[i].coord.lon && data[i].coord.lat) {
-              let id = data[i].id;
-              let name = data[i].name;
-              let country = data[i].country;
-              let lon = data[i].coord.lon;
-              let lat = data[i].coord.lat;
-              this.arrData = [id, name, country, lon, lat];
-              this.results.push(this.arrData);
-            }
-            
-       }
-        
-    }
-    this.cityData = this.results
-    console.log(this.cityData);  
-    });
-  }
+  selectData = [];
+  toLon:number = 77.2;
+  toLat:number = 28.6;
+  cityId:number = 1261481;
 
-  ngOnInit() {
-   
-    let cityWeather = this.httpClient.get('http://api.openweathermap.org/data/2.5/find?lat=22.0&lon=82.6&cnt=40&mode=xml&appid=79cce9d1cd2fb9e584cca5a598f53932',{responseType:"text"});
-    let dayWeather = this.httpClient.get('https://cors-anywhere.herokuapp.com/http://samples.openweathermap.org/data/2.5/forecast?id=1274837&appid=79cce9d1cd2fb9e584cca5a598f53932');
-   
+  //For getting weather detail
+  getWeather() {
+    this.tableData =[];
+    console.log(this.cityId+" "+this.toLat+" "+this.toLon);
+    let cityWeather = this.httpClient.get('http://api.openweathermap.org/data/2.5/find?lat='+this.toLat+'&lon='+this.toLon+'&cnt=3&mode=xml&appid=79cce9d1cd2fb9e584cca5a598f53932',{responseType:"text"});
+    let dayWeather = this.httpClient.get('https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast?id='+this.cityId+'&appid=79cce9d1cd2fb9e584cca5a598f53932');
     forkJoin([cityWeather,dayWeather]).subscribe(
     (response =>{
-      //Convert Xml string to JSON
-      this.weatherData = this.x2js.xml_str2json(response[0]);  
+      this.weatherData = this.x2js.xml_str2json(response[0]); //Convert Xml string to JSON 
     
       //To access all array item for city weather
       this.weatherData.cities.list.item.forEach((item, i:number) => {
@@ -77,7 +57,7 @@ export class WeatherComponent implements OnInit {
          this.temp = this.weatherData.cities.list.item[i].temperature._value;
          this.arr =[this.cityName,this.weatherDes,this.sunRise,this.sunSet,this.temp];
          this.tableData.push(this.arr);  
-        });
+      });
       this.detail1= this.tableData;
 
       //To access all array item for day wise weather
@@ -92,13 +72,49 @@ export class WeatherComponent implements OnInit {
         this.tdata.push(arr);
       });
        this.detail2 = this.tdata;
-        //console.log(this.tdata);
     })  
     );
+  }
 
-  //To get table row description
-  $(document).on("click","tr#getData", function(e){
-    //alert(e.target.innerHTML);
+  //To search city detail from local json file
+  searchCity(){
+    this.results=[];
+    let city:string = $('#city').val();
+    var searchField = "name";
+    this.dataFinder.getJSONDataAsync("./assets/data/city.list.json").then(data => {
+    for (var i=0 ; i < data.length ; i++) {
+        if (data[i]['name'] == city) {
+            if(data[i].id && data[i].name && data[i].country && data[i].coord.lon && data[i].coord.lat) {
+              let id = data[i].id;
+              let name = data[i].name;
+              let country = data[i].country;
+              let lon = data[i].coord.lon;
+              let lat = data[i].coord.lat;
+              this.arrData = [id, name, country, lon, lat];
+              this.results.push(this.arrData);
+          }           
+       }      
+    }
+    this.cityData = this.results  
+    });
+  }
+
+  //To select city detail
+  onSelect(selectData){
+   this.selectData = selectData;
+   this.cityId = this.selectData[0];
+   this.toLon = this.selectData[3].toFixed(1);
+   this.toLat = this.selectData[4].toFixed(1);
+   this.getWeather();
+
+  }
+  
+   
+
+  ngOnInit() { 
+    this.getWeather();
+  //Clickable table row to get row description
+  $(document).on("click","tr#getData", function(e) {
     $(this).siblings().slideToggle(20);
  });
   }
