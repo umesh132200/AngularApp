45249fe99199a7caa1c7e580e1fca3bdb0e32ed9
Umesh Kumar <umesh132200@gmail.com>
2018-02-21 00:40:20 +0530

    weather.component.ts cleaned code

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -14,72 +14,46 @@ import { Subject } from 'rxjs/Subject';
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent implements OnInit {
-  dtOptions: any = {};
-  dtTrigger: Subject<any> = new Subject();
+
   constructor(private httpClient:HttpClient, private dataRequest:DataRequestService, private router:Router) {}
+  dtTrigger: Subject<any> = new Subject();
   apikey:string = '79cce9d1cd2fb9e584cca5a598f53932';
-  form;
-  msg:any= "";
-  city:string="";
-  arr:any[];
-  detail1:any;
-  cityWeather:any;
-  detail2:any[];
-  cityName:any[];
-  cityIds:any[];
-  weatherDes:any[];
-  sunRise:any[];
-  sunSet:any[];
-  temp:any[];
-  tableData:any[] = new Array();
-  mulResponse:any[] = new Array();
-  tdata:any[] = new Array();  
-  weatherData:any='';
-  cityWiseData:any=''; 
-  id = [];
-  name = [];
-  country = [];
-  coord = [];
-  results = [];
-  cityData = [];
-  arrData =[];
-  selectData = [];
   toLon:number = 77.2;
   toLat:number = 28.6;
   cityId:number = 1261481;
-  order:boolean = false;
-  field:any;
-  fiveDayWeather:any;
+  form:FormGroup;
+  dtOptions: any = {};
+  detail1:any;
+  cityWeather:any;
+  cityData = [];
+  msg:string= "";
+ 
 
   /**This method is used to get "current weather" data of all city in latitute & longitute circle
    * and getting data from openWeatherMap api. 
    */
   getWeather() {
-    this.mulResponse = [];
     this.dataRequest.sendRequest('https://api.openweathermap.org/data/2.5/find?lat='+this.toLat+'&lon='+this.toLon+'&cnt=40&appid='+this.apikey)
-    .then(data => {
-      this.cityWeather = data.list;
-      this.dtTrigger.next();
-      this.getDayWise(this.cityWeather);
-    },
-    error => {}
-  ).catch(this.handleError);
- }
+    .then(
+      data => {
+        this.cityWeather = data.list;
+        this.dtTrigger.next();
+        this.getDayWise(this.getCityIds(this.cityWeather));
+      },
+      error => {this.handleError}
+    ).catch(this.handleError);
+  }
 
+   
   /**This method is used to get "5 day/ 3 hour weather" data of selected city
    * and getting data from openWeatherMap api. 
    */
-  getDayWise(cityweather){
-    var arr=[];
-    cityweather.forEach(element => {
-      arr.push(element.id);
-    });
-
-    this.dataRequest.getResult(arr) 
-    .subscribe(data => { 
-      this.detail1 = data;
-    },
-  error => {this.handleError});        
+  getDayWise(id) {
+    this.dataRequest.getResult(id) 
+    .subscribe(
+      data => this.detail1 = data,
+      error => this.handleError
+    );        
   }
   
 
@@ -88,11 +62,9 @@ export class WeatherComponent implements OnInit {
    * to five-day-weather component via service.
    */
   getFiveDayWeather(id){
-    var data:any;
     this.detail1.forEach(element => {
-      if(element.city.id === id){
-        data = element;
-        this.dataRequest.dataCast(data);
+      if(element.city.id === id) {
+        this.dataRequest.dataCast(element);
         this.router.navigate(['/five-day-weather']);
       }
     });
@@ -104,30 +76,41 @@ export class WeatherComponent implements OnInit {
    */
   searchCity(ct) {
     this.msg = "";
-    $('form:nth-child(1)').removeClass('has-error');//remove error class
+    $('form:nth-child(1)').removeClass('has-error');
     if(ct.cityname) {
-      this.results=[];
-      let city:string = ct.cityname.trim().replace(/(^|\s)\S/g, l => l.toUpperCase());
+      let city:string = this.getCleanString(ct.cityname);
       this.dataRequest.sendRequest('https://api.openweathermap.org/data/2.5/find?q='+city+'&type=accurate&appid='+this.apikey)
-      .then(data => {
-        this.results = data.list;
-        if(this.results.length == 0) {
-         this.msg = "City doesn't found!";
-         $('[name=cityname]').val(''); 
-        }
-        else {
-          this.cityData = this.results  
-        }
-      },
-    error => {this.handleError});
+      .then(
+        data => {
+            if(data.list.length == 0) {
+              this.msg = "City doesn't found!";
+              $('[name=cityname]').val(''); 
+            } 
+            else { this.cityData = data.list }
+          },
+        error => {this.handleError})
+      .catch(this.handleError);
+    } 
+    else {
+        $('form:nth-child(1)').addClass('has-error');
+        this.msg = "Please enter city name!";
+      }
     }
-    else{
-      $('form:nth-child(1)').addClass('has-error');
-      this.msg = "Please enter city name!";
-    }
-   
-  }
+  
+   //Clean user input
+   getCleanString(cityname){
+     return cityname.trim().replace(/(^|\s)\S/g, l => l.toUpperCase());
+   }
 
+   //To get all city id
+   getCityIds(data){
+    var arr=[];
+    data.forEach(element => {
+      arr.push(element.id);
+    });
+    return arr;
+  }
+  
   //error handling
   public handleError = (error:Response) => {
     return Observable.throw(error);
@@ -136,7 +119,7 @@ export class WeatherComponent implements OnInit {
   /**This method is used to select city 
    * and request for current weather data from openWeatherMap api 
    */
-  onSelect(selectData){
+  onSelect(selectData) {
    this.cityId = selectData.id;
    let lon:string = parseFloat(selectData.coord.lon).toFixed(4);
    let lat:string = parseFloat(selectData.coord.lat).toFixed(4);
@@ -145,26 +128,14 @@ export class WeatherComponent implements OnInit {
    this.getWeather();
   }
   
-  ngOnInit() { 
+  ngOnInit() {   
+    this.getWeather(); //onLoad current city weather.
 
-    this.dtOptions = { 
-      responsive: true
-    }; 
+    this.dtOptions = { responsive: true }; //For datatable responsive
 
-    //Load default city to get current weather.
-    this.getWeather();
-
-    
-    //Clickable table row to get row description
-    $(document).on("click","#slidetoggle", function(e) {
-      $(this).siblings().slideToggle('slow');
-    });
-     
     //To validate the search box
     this.form = new FormGroup({
-      cityname : new FormControl("", Validators.compose([
-        Validators.required
-      ])) 
+      cityname : new FormControl("", Validators.compose([Validators.required])) 
     });
   }
 
